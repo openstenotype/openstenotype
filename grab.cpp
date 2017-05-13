d19e2635546eb4d337c04126a6485be08a6ea8f2
Henry Hirsch <henry@w3-net.de>
2017-05-13 18:37:36 +0200

    Add first keybinding

--- grab.cpp
+++ grab.cpp
@@ -15,15 +15,10 @@ using namespace std;
 struct StenoBoard {
 public:
   //14 buttons per side
-  bool s = false;
-  bool t = false;
-  bool p = false;
-  bool h = false;
-  bool k = false;
-  bool w = false;
-  bool r = false;
-  bool a = false;
-  bool o = false;
+  bool s, t, p, h, k, w, r ,a ,o = false;
+  void resetButtons(){
+    s, t, p, h, k, w, r ,a ,o = false;
+  }
 };
 
 
@@ -55,6 +50,22 @@ XKeyEvent createKeyEvent(Display *display, Window &win,
    return event;
 }
 
+void simulateKeypress(Display* display, int key) {
+  Window winRoot = XDefaultRootWindow(display);
+  Window winFocus;
+  int revert;
+  XGetInputFocus(display, &winFocus, &revert);
+
+  // Send a fake key press event to the window.
+  XKeyEvent event = createKeyEvent(display, winFocus, winRoot, true,  key, 0);
+  XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
+  std::this_thread::sleep_for(std::chrono::milliseconds(2));
+  // Send a fake key release event to the window.
+  event = createKeyEvent(display, winFocus, winRoot, false, key, 0);
+  XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
+  // For key codes check http://www.cl.cam.ac.uk/~mgk25/ucs/keysymdef.h
+
+}
 
 int main(void)
 {
@@ -69,31 +80,14 @@ int main(void)
   XGrabKeyboard(display, DefaultRootWindow(display), false, 
                 GrabModeAsync, GrabModeAsync,t);
   int presses = 0;
-
-  Window winFocus;
-  int    revert;
   KeySym key;
 
   while(presses < 10)
     {
-
-      // Window winRoot = XDefaultRootWindow(display);
-      // XGetInputFocus(display, &winFocus, &revert);
-
-      // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-      // // Send a fake key press event to the window.
-      // XKeyEvent event = createKeyEvent(display, winFocus, winRoot, true,  XK_M, 0);
-      // XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
-
-      // // Send a fake key release event to the window.
-      // event = createKeyEvent(display, winFocus, winRoot, false, XK_M, 0);
-      // XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
-      // For key codes check http://www.cl.cam.ac.uk/~mgk25/ucs/keysymdef.h
       XNextEvent(display, &event);
       switch(event.type)
         {
         case KeyPress:
-          cout << "Key pressed" << endl;
           presses++;
           key = XkbKeycodeToKeysym( display, event.xkey.keycode, 
                                     0, event.xkey.state & ShiftMask ? 1 : 0);
@@ -124,15 +118,15 @@ int main(void)
           //          XSendEvent(display,InputFocus,True,KeyReleaseMask,&ev);
           key = XkbKeycodeToKeysym( display, event.xkey.keycode, 
                                 0, event.xkey.state & ShiftMask ? 1 : 0);
-          cout << "Key code" << endl;
           if (stenoboard.k && stenoboard.w && stenoboard.r) {
             cout << "Exiting" << endl;
             exit(1);
           }
 
           if (stenoboard.t && stenoboard.o) {
-            cout << "Exiting" << endl;
-            exit(1);
+            simulateKeypress(display, XK_t);
+            simulateKeypress(display, XK_o);
+            stenoboard.resetButtons();
           }
 
           break;
