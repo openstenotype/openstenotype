f0346d4cd21153f0cacafaf557d85200c547b2f9
Henry Hirsch <henry@w3-net.de>
2017-05-16 21:31:43 +0200

    Very basic functionality in WindowSystem

--- include/opensteno/WindowSystem.hpp
+++ include/opensteno/WindowSystem.hpp
@@ -1,10 +1,27 @@
 #ifndef OPENSTENO_WINDOW_SYSTEM_HPP
 #define OPENSTENO_WINDOW_SYSTEM_HPP
+#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Intrinsic.h>
+#include <X11/extensions/XTest.h>
+#include <X11/XKBlib.h>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+
+
 namespace opensteno {
 class WindowSystem {
 public:
   WindowSystem();
+  ~WindowSystem();
   void createKeyEvent();
+private:
+  Display* display;
+  void grabKeyboard();
 };
 } /* namespace opensteno */
 #endif //OPENSTENO_WINDOW_SYSTEM_HPP
--- source/WindowSystem.cpp
+++ source/WindowSystem.cpp
@@ -2,8 +2,24 @@
 
 namespace opensteno {
   WindowSystem::WindowSystem() {
+    display = XOpenDisplay(0x0);
+    if(!display) {
+      std::cout << "No screen found!" << std::endl;
+      exit(1);
+    };
+
+    grabKeyboard();
   }
-  
+
+  WindowSystem::~WindowSystem() {
+    XCloseDisplay(display);
+  }
+
+  void WindowSystem::grabKeyboard() {
+    XGrabKeyboard(display, DefaultRootWindow(display), false,
+                  GrabModeAsync, GrabModeAsync, CurrentTime);
+  }
+
   void WindowSystem::createKeyEvent() {
   }
 } /* namespace opensteno */
