7d452c4387a79fd3e86bfde8ceeac098158ec698
Henry Hirsch <henry@w3-net.de>
2017-05-24 23:45:01 +0200

    Rename some keys in stenoboard model

--- include/opensteno/Stenoboard.hpp
+++ include/opensteno/Stenoboard.hpp
@@ -2,16 +2,16 @@
 #define OPENSTENO_STENOBOARD_HPP
 namespace opensteno {
 struct StenoboardLeft {
-  bool numberBar1;
+  bool numberBar;
   bool fn1, s1, t, p, h, asterisk1;
   bool fn2, s2, k, w, r, asterisk2;
   bool a, o;
 };
 
 struct StenoboardRight {
-  bool numberBar2;
-  bool asterisk3, f, p, l, t, d;
-  bool asterisk4, r, b, g, s, z;
+  bool numberBar;
+  bool asterisk1, f, p, l, t, d;
+  bool asterisk2, r, b, g, s, z;
   bool e, u;
 };
 
@@ -25,15 +25,15 @@ public:
     resetRightButtons();
   }
   void resetLeftButtons(){
-    left.numberBar1 = false;
+    left.numberBar = false;
     left.fn1 = left.s1 = left.t = left.p = left.h = left.asterisk1 = false;
     left.fn2 = left.s2 = left.k = left.w = left.r = left.asterisk2 = false;
     left.a = left.o = false;
   }
   void resetRightButtons(){
-    right.numberBar2 = false;
-    right.asterisk3 = right.f = right.p = right.l = right.t = right.d = false;
-    right.asterisk4 = right.r = right.b = right.g = right.s = right.z = false;
+    right.numberBar = false;
+    right.asterisk1 = right.f = right.p = right.l = right.t = right.d = false;
+    right.asterisk2 = right.r = right.b = right.g = right.s = right.z = false;
     right.e = right.u = false;
   }
 };
