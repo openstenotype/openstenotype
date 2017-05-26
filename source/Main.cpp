6125ad601c37c22787ce8fb21be80b7fe1e08310
Henry Hirsch <henry@w3-net.de>
2017-05-26 15:06:08 +0200

    Add very basic qt ui

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,11 +1,30 @@
+#include <QApplication>
+#include <QLabel>
+#include <QMessageBox>
+#include <QStatusBar>
+#include <QDockWidget>
 #include <opensteno/Opensteno.hpp>
+#include <thread>
 
 using namespace opensteno;
 
-int main(void)
+void runUi(int argc, char **argv)
+{
+  QApplication app(argc, argv);
+  QLabel *label = new QLabel("Opensteno");
+  label->setGeometry( 100,100,500,500 );
+  label->show();
+  app.exec();
+}
+
+int main(int argc, char **argv)
 {
   Opensteno opensteno;
+  std::thread uiThread(runUi, argc, argv);
   while(opensteno.isRunning()) {
     opensteno.driver.update();
   }
+  QApplication::quit();
+  uiThread.join();
+  return 0;
 }
--- source/Opensteno.cpp
+++ source/Opensteno.cpp
@@ -2,7 +2,6 @@
 
 namespace opensteno {
   Opensteno::Opensteno():driver(windowSystem) {
-
   }
 
   bool Opensteno::isRunning() {
